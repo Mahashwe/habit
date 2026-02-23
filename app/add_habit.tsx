abcba50bd45eddbe86570428a52a35365d953933
Mahashwe <maha.tomholland@gmail.com>
2026-02-23 12:30:05 +0530

    implement AddHabit component with input fields and submit functionality

--- app/add_habit.tsx
+++ app/add_habit.tsx
@@ -1,10 +1,81 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, StyleSheet, Pressable } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
+
 const AddHabit = () => {
+  const [habitName, setHabitName] = useState("");
+  const [habitDescription, setHabitDescription] = useState("");
+
+  const handleSubmit = () => {
+    console.log(habitName, habitDescription);
+  };
+
   return (
-    <View>
-      <Text>Add Habit Screen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.form}>
+        <Text style={styles.text}>Add Your Habit</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter habit name"
+            value={habitName}
+            onChangeText={setHabitName}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Enter habit description"
+            value={habitDescription}
+            onChangeText={setHabitDescription}
+          />
+          <Pressable style={styles.button} onPress={handleSubmit}>
+            <Text style={styles.buttonText}>Submit</Text>
+          </Pressable>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default AddHabit;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  form: {
+    marginTop: 20,
+    borderRadius: 20,
+    borderWidth: 3,
+    borderColor: "#5faafaff",
+    padding: 16,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  inputContainer: {
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "#5faafaff",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
