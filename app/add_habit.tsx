113ec0fc03ef7d21dc6abe54d8ce77000efc6a33
Mahashwe <maha.tomholland@gmail.com>
2026-02-23 12:09:12 +0530

    add navigation to add habit screen; implement AddHabit component

---
+++ app/add_habit.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+const AddHabit = () => {
+  return (
+    <View>
+      <Text>Add Habit Screen</Text>
+    </View>
+  );
+};
+
+export default AddHabit;
--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,45 @@
-import {Text, View, StyleSheet} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import { Text, View, StyleSheet, Button, Pressable } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
 
 export default function App() {
+  const navigation = useNavigation();
+
   return (
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.text}>Hello, World!</Text>
-      </SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Hello, World!</Text>
+      <Pressable
+        style={styles.button}
+        onPress={() => router.push("/add_habit")}
+      >
+        <Text style={styles.buttonText}>+ Add Habit</Text>
+      </Pressable>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   text: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+  },
+  button: {
+    marginTop: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: "#5faafaff",
+    borderRadius: 5,
+    marginRight: 250,
+    marginLeft: "1%",
+    alignItems: "flex-start",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
